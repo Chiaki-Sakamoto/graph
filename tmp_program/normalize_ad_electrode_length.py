0f25a729f62cb1c1e60c0b17f2f1655d84e0bb9e
deco-L <scotchwitchbroom@gmail.com>
2024-02-02 20:21:38 +0900

    V V

---
+++ tmp_program/normalize_ad_electrode_length.py
@@ -0,0 +1,154 @@
+import sys
+import re
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+
+
+class BAND:
+    def __init__(self, Gband, Yband, Zband):
+        self.gband = Gband
+        self.yband = Yband
+        self.zband = Zband
+
+
+class ELECTRODE_LEN:
+    def __init__(self, E10mm, E30mm, E50mm):
+        self.e10mm = E10mm
+        self.e30mm = E30mm
+        self.e50mm = E50mm
+
+
+def gauss(x, a, mu, sigma):
+    return a * np.exp(-(x - mu) ** 2 / (2 * sigma ** 2))
+
+
+def fit(func, x, y, param_init):
+    popt, pocv = curve_fit(func, x, y, param_init)
+    perr = np.sqrt(np.diag(pocv))
+    return popt, pocv, perr
+
+
+def _branch_band_path(argv):
+    gband = list()
+    yband = list()
+    zband = list()
+    for path in argv:
+        if "G-band" in path:
+            gband.append(path)
+        if "Y-band" in path:
+            yband.append(path)
+        if "Z-band" in path:
+            zband.append(path)
+    return gband, yband, zband
+
+
+def _branch_electrode_len(band_path):
+    e10mm = list()
+    e30mm = list()
+    e50mm = list()
+    for path in band_path:
+        if "L=10mm" in path:
+            e10mm.append(path)
+        if "L=30mm" in path:
+            e30mm.append(path)
+        if "L=50mm" in path:
+            e50mm.append(path)
+    return e10mm, e30mm, e50mm
+
+
+def _init_angle_distribution(paths):
+    angle_distribution = dict()
+    for index, path in enumerate(paths):
+        if not path.endswith(".txt"):
+            print(
+                "Error\n"
+                f"{path}: Extension is not .txt\n"
+                )
+            continue
+        match = re.search(r'(?<=_)([-\d]+)deg', path)
+        angle = int(match.group(1))
+        time, signal = np.loadtxt(
+            path, skiprows=3, unpack=True, delimiter=','
+            )
+        max_signal = -min(signal)
+        angle_distribution[angle] = max_signal
+    return angle_distribution
+
+
+def _sort_angle_distribution(angle_distribution):
+    sorted_angle_distribution = dict(
+        sorted(
+            angle_distribution.items(),
+            key=lambda x: x[0],
+            reverse=True
+            )
+        )
+    return sorted_angle_distribution
+
+
+def main():
+    argv = sys.argv[1:]
+    band_path = BAND(*_branch_band_path(argv))
+    for band in ("gband", "yband", "zband"):
+        fig, axs = plt.subplots(layout="tight")
+        axs.set_xlabel("Angle (degree)", fontsize=30)
+        axs.set_ylabel("Signal voltage (mV)", fontsize=30)
+        axs.set_xlim(-35, 35)
+        axs.set_xticks(np.arange(-35, 36, 5))
+        axs.get_xaxis().set_tick_params(pad=15)
+        axs.get_yaxis().set_tick_params(pad=5)
+        axs.set_ylim(0, 1.2)
+        if band == "gband":
+            axs.set_title("0.14 ~ 0.22 THz", fontsize=30, pad=20)
+        if band == "yband":
+            axs.set_title("0.22 ~ 0.33 THz", fontsize=30, pad=20)
+        if band == "zband":
+            axs.set_title("0.33 ~ 0.50 THz", fontsize=30, pad=20)
+        for length in ("10mm", "30mm", "50mm"):
+            if length == "10mm":
+                color = 'r'
+            if length == "30mm":
+                color = 'g'
+            if length == "50mm":
+                color = 'b'
+            electrode_len = ELECTRODE_LEN(
+                *_branch_electrode_len(getattr(band_path, band))
+                )
+            ad_electrode_length = _init_angle_distribution(getattr(electrode_len, "e" + length))
+            sorted_angle_distribution = _sort_angle_distribution(ad_electrode_length)
+            x_list = list(sorted_angle_distribution.keys())
+            y_list = list(sorted_angle_distribution.values())
+            x = np.array(x_list)
+            y = np.array(y_list)
+            y_max = max(y)
+            gauss_fit = fit(gauss, x, y * 10 ** 3, [np.max(y) * 10 ** 3, np.argmax(y), np.std(y * 10 ** 3)])
+            fit_x = np.linspace(min(x) - 5, max(x) + 5, 1000)
+            fit_y = gauss(fit_x, *gauss_fit[0])
+            fit_y_max = max(fit_y)
+            if not (band == "zband" and length == "10mm"):
+                axs.plot(
+                    fit_x,
+                    fit_y / fit_y_max,
+                    color + '-',
+                    linewidth=2,
+                    )
+            axs.plot(
+                x,
+                y / y_max,
+                color + 'o',
+                label=length
+                )
+        axs.legend(
+            bbox_to_anchor=(1.18, 1),
+            loc="upper right",
+            borderaxespad=0
+        )
+        plt.savefig("/tmp/" + f"{band}_gaussian_fitting_ad_ele_len" + ".pdf", format="pdf")
+        plt.show()
+        plt.close()
+    return
+
+
+if __name__ == "__main__":
+    main()
---
+++ tmp_program/normalize_ad_rayleigh_length.py
@@ -0,0 +1,345 @@
+import re
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+
+
+class BAND_PATH:
+    def __init__(self, G_band_path, Y_band_path, Z_band_path):
+        self.gband = G_band_path
+        self.yband = Y_band_path
+        self.zband = Z_band_path
+
+
+class AD_LIST:
+    def __init__(self, F200_ad, F400_ad, F800_ad):
+        self.ad_f200 = F200_ad
+        self.ad_f400 = F400_ad
+        self.ad_f800 = F800_ad
+
+
+def gauss(x, a, mu, sigma):
+    return a * np.exp(-(x - mu) ** 2 / (2 * sigma ** 2))
+
+
+def fit(func, x, y, param_init):
+    popt, pocv = curve_fit(func, x, y, param_init)
+    perr = np.sqrt(np.diag(pocv))
+    return popt, pocv, perr
+
+
+def _init_angle_distribution(band_path):
+    angle_distribution = dict()
+    for index, path in enumerate(band_path):
+        if not path.endswith(".txt"):
+            print(
+                "Error\n"
+                f"{path}: Extension is not .txt\n"
+                )
+            continue
+        match = re.search(r'(?<=_)([-\d]+)deg', path)
+        angle = int(match.group(1))
+        time, signal = np.loadtxt(
+            path, skiprows=3, unpack=True, delimiter=','
+            )
+        max_signal = -min(signal)
+        angle_distribution[angle] = max_signal
+    return angle_distribution
+
+
+def _sort_angle_distribution(angle_distribution):
+    sorted_angle_distribution = dict(
+        sorted(
+            angle_distribution.items(),
+            key=lambda x: x[0],
+            reverse=True
+            )
+        )
+    return sorted_angle_distribution
+
+
+def _branch_set_band_fig(band, axs):
+    axs.set_ylim(0, 1.2)
+    if band == "gband":
+        axs.set_title("0.14 ~ 0.22 THz", fontsize=30, pad=20)
+    if band == "yband":
+        axs.set_title("0.22 ~ 0.33 THz", fontsize=30, pad=20)
+    if band == "zband":
+        axs.set_title("0.33 ~ 0.50 THz", fontsize=30, pad=20)
+
+
+def _plot_focal_band_ad(band_ad_list, band, axs):
+    for focal in ("200mm", "400mm", "800mm"):
+        if focal == "200mm":
+            focal_band_ad = band_ad_list.ad_f200
+            color = 'r'
+        if focal == "400mm":
+            focal_band_ad = band_ad_list.ad_f400
+            color = 'g'
+        if focal == "800mm":
+            focal_band_ad = band_ad_list.ad_f800
+            color = 'b'
+        label = "f = " + focal
+        x_list = list(focal_band_ad.keys())
+        y_list = list(focal_band_ad.values())
+        x = np.array(x_list)
+        y = np.array(y_list)
+        y_max = max(y)
+        peak_index = np.argmax(y)
+        peak_value = np.max(y) * 10 ** 3
+        std = np.std(y * 10 ** 3)
+        print(f"std{std}, peak_index{peak_index}, peak_value{peak_value}")
+        gauss_fit = fit(gauss, x, y * 10 ** 3, [peak_value, peak_index, std])
+        fit_x = np.linspace(min(x) - 5, max(x) + 5, 1000)
+        fit_y = gauss(fit_x, *gauss_fit[0])
+        fit_y_max = max(fit_y)
+        if not (band == "zband" and focal == "200mm"):
+            axs.plot(
+                fit_x,
+                fit_y / fit_y_max,
+                color + '-',
+                linewidth=2,
+                )
+        axs.plot(
+            x,
+            y / y_max,
+            color + 'o',
+            label=label,
+        )
+    axs.legend(
+        bbox_to_anchor=(1.25, 1),
+        loc="upper right",
+        borderaxespad=0
+    )
+
+
+def _set_label_limit(axs):
+    axs.set_xlabel("Angle (degree)", fontsize=30)
+    axs.set_xlim(-35, 35)
+    axs.set_xticks(np.arange(-35, 36, 5))
+    axs.get_xaxis().set_tick_params(pad=15)
+    axs.set_ylabel("Signal voltage (mV)", fontsize=30)
+    axs.get_yaxis().set_tick_params(pad=5)
+
+
+def branch_focal_path(argv):
+    result = dict(
+        f200mm=list(),
+        f400mm=list(),
+        f800mm=list(),
+    )
+    for path in argv:
+        if "Lens" in path and "200mm" in path:
+            result["f200mm"].append(path)
+        if "Lens" in path and "400mm" in path:
+            result["f400mm"].append(path)
+        if "Lens" in path and "800mm" in path:
+            result["f800mm"].append(path)
+    return result
+
+
+def branch_band_path(focal_path):
+    gband = list()
+    yband = list()
+    zband = list()
+    for path in focal_path:
+        if "G-band" in path:
+            gband.append(path)
+        if "Y-band" in path:
+            yband.append(path)
+        if "Z-band" in path:
+            zband.append(path)
+    return gband, yband, zband
+
+
+def main():
+    argv = sys.argv[1:]
+    focal_path = branch_focal_path(argv)
+    f200_path = BAND_PATH(*branch_band_path(focal_path["f200mm"]))
+    f400_path = BAND_PATH(*branch_band_path(focal_path["f400mm"]))
+    f800_path = BAND_PATH(*branch_band_path(focal_path["f800mm"]))
+    for band in ("gband", "yband", "zband"):
+        fig, axs = plt.subplots(layout="tight")
+        _set_label_limit(axs)
+        _branch_set_band_fig(band, axs)
+        band_ad_list = AD_LIST(
+            _sort_angle_distribution(
+                _init_angle_distribution(getattr(f200_path, band))
+                ),
+            _sort_angle_distribution(
+                _init_angle_distribution(getattr(f400_path, band))
+                ),
+            _sort_angle_distribution(
+                _init_angle_distribution(getattr(f800_path, band))
+                ),
+        )
+        _plot_focal_band_ad(band_ad_list, band, axs)
+        plt.savefig("/tmp/" + band + "_gaussian_fitting_ad_ray_len.pdf", format="pdf")
+        plt.show()
+        plt.close()
+
+
+if __name__ == "__main__":
+    main()
+
+
+# import re
+# import sys
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+
+# class BAND_PATH:
+#     def __init__(self, G_band_path, Y_band_path, Z_band_path):
+#         self.gband = G_band_path
+#         self.yband = Y_band_path
+#         self.zband = Z_band_path
+
+
+# class AD_LIST:
+#     def __init__(self, F200_ad, F400_ad, F800_ad):
+#         self.ad_f200 = F200_ad
+#         self.ad_f400 = F400_ad
+#         self.ad_f800 = F800_ad
+
+
+# def _init_angle_distribution(band_path):
+#     angle_distribution = dict()
+#     for index, path in enumerate(band_path):
+#         if not path.endswith(".txt"):
+#             print(
+#                 "Error\n"
+#                 f"{path}: Extension is not .txt\n"
+#                 )
+#             continue
+#         match = re.search(r'(?<=_)([-\d]+)deg', path)
+#         angle = int(match.group(1))
+#         time, signal = np.loadtxt(
+#             path, skiprows=3, unpack=True, delimiter=','
+#             )
+#         max_signal = -min(signal)
+#         angle_distribution[angle] = max_signal
+#     return angle_distribution
+
+
+# def _sort_angle_distribution(angle_distribution):
+#     sorted_angle_distribution = dict(
+#         sorted(
+#             angle_distribution.items(),
+#             key=lambda x: x[0],
+#             reverse=True
+#             )
+#         )
+#     return sorted_angle_distribution
+
+
+# def _branch_set_band_fig(band, axs):
+#     axs.set_ylim(0, 1.2)
+#     if band == "gband":
+#         axs.set_title("0.14 ~ 0.22 THz", fontsize=30, pad=20)
+#     if band == "yband":
+#         axs.set_title("0.22 ~ 0.33 THz", fontsize=30, pad=20)
+#     if band == "zband":
+#         axs.set_title("0.33 ~ 0.50 THz", fontsize=30, pad=20)
+
+
+# def _plot_focal_band_ad(band_ad_list, axs):
+#     for focal in ("200mm", "400mm", "800mm"):
+#         if focal == "200mm":
+#             focal_band_ad = band_ad_list.ad_f200
+#             color = 'r'
+#         if focal == "400mm":
+#             focal_band_ad = band_ad_list.ad_f400
+#             color = 'g'
+#         if focal == "800mm":
+#             focal_band_ad = band_ad_list.ad_f800
+#             color = 'b'
+#         label = "f = " + focal
+#         x = np.array(list(focal_band_ad.keys()))
+#         y = np.array(list(focal_band_ad.values()))
+#         y_max = max(y)
+#         axs.plot(
+#             x,
+#             y / y_max,
+#             color + 'o',
+#             label=label,
+#         )
+#     axs.legend(
+#         bbox_to_anchor=(1.25, 1),
+#         loc="upper right",
+#         borderaxespad=0
+#     )
+
+
+# def _set_label_limit(axs):
+#     axs.set_xlabel("Angle (degree)", fontsize=30)
+#     axs.set_ylabel("Signal voltage (mV)", fontsize=30)
+#     axs.set_xlim(-35, 35)
+#     axs.set_xticks(np.arange(-35, 36, 5))
+
+
+# def branch_focal_path(argv):
+#     result = dict(
+#         f200mm=list(),
+#         f400mm=list(),
+#         f800mm=list(),
+#     )
+#     for path in argv:
+#         if "Lens" in path and "200mm" in path:
+#             result["f200mm"].append(path)
+#         if "Lens" in path and "400mm" in path:
+#             result["f400mm"].append(path)
+#         if "Lens" in path and "800mm" in path:
+#             result["f800mm"].append(path)
+#     return result
+
+
+# def branch_band_path(focal_path):
+#     gband = list()
+#     yband = list()
+#     zband = list()
+#     for path in focal_path:
+#         if "G-band" in path:
+#             gband.append(path)
+#         if "Y-band" in path:
+#             yband.append(path)
+#         if "Z-band" in path:
+#             zband.append(path)
+#     return gband, yband, zband
+
+
+# def main():
+#     argv = sys.argv[1:]
+#     focal_path = branch_focal_path(argv)
+#     f200_path = BAND_PATH(*branch_band_path(focal_path["f200mm"]))
+#     f400_path = BAND_PATH(*branch_band_path(focal_path["f400mm"]))
+#     f800_path = BAND_PATH(*branch_band_path(focal_path["f800mm"]))
+#     for band in ("gband", "yband", "zband"):
+#         fig, axs = plt.subplots(layout="tight")
+#         axs.get_xaxis().set_tick_params(pad=15)
+#         axs.get_yaxis().set_tick_params(pad=5)
+#         _set_label_limit(axs)
+#         _branch_set_band_fig(band, axs)
+#         band_ad_list = AD_LIST(
+#             _sort_angle_distribution(
+#                 _init_angle_distribution(getattr(f200_path, band))
+#                 ),
+#             _sort_angle_distribution(
+#                 _init_angle_distribution(getattr(f400_path, band))
+#                 ),
+#             _sort_angle_distribution(
+#                 _init_angle_distribution(getattr(f800_path, band))
+#                 ),
+#         )
+#         _plot_focal_band_ad(band_ad_list, axs)
+#         plt.savefig(
+#             "/tmp/" + f"{band}_ad_rayleigh_length" + ".pdf",
+#             format="pdf"
+#             )
+#         plt.show()
+#         plt.close()
+
+
+# if __name__ == "__main__":
+#     main()
